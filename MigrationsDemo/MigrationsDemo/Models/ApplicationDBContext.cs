29f018e21cb61a2eb8d1cb512b191cef0ff67017
StarPlatinu <thanhvp1509@gmail.com>
2023-03-21 08:55:46 +0700

    migration

---
+++ MigrationsDemo/MigrationsDemo/Models/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MigrationsDemo.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MigrationsDemo.Models.Products> Products { get; set; } = default!;
+    }
+}
+
---
+++ MigrationsDemo/MigrationsDemo/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MigrationsDemo.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public string Category { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockQty { get; set; }
+    }
+}
